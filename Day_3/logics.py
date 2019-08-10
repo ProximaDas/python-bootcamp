5e1ba5178ac99023436d1c70965be1ca903d5475
Michael Gutensohn <michael@gutensohn.me>
2019-08-09 21:40:56 -0700

    Day 3 slides in ipynb form.

---
+++ Day_3/logics.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# HW05_ex00_logics.py
+
+
+##############################################################################
+def even_odd():
+    """ Print even or odd:
+        Takes one integer from user
+            accepts only non-word numerals
+            must validate
+        Determines if even or odd
+        Prints determination
+        returns None
+    """
+    pass
+
+
+def ten_by_ten():
+    """ Prints integers 1 through 100 sequentially in a ten by ten grid."""
+    pass
+
+
+def find_average():
+    """ Takes numeric input (non-word numerals) from the user, one number
+    at a time. Once the user types 'done', returns the average.
+    """
+    pass
+
+
+##############################################################################
+def main():
+    """ Calls the following functions:
+        - even_odd()
+        - ten_by_ten()
+    Prints the following function:
+        - find_average()
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
